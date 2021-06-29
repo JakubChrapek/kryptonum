b1c6680fcaee8f0007a4049e8079b1ef0250bd88
jakubchrapek <chrapekuba@gmail.com>
2021-06-29 06:33:05 +0200

    Canvas tests

--- src/components/organisms/HomeHero/HeroCanvas.js
+++ src/components/organisms/HomeHero/HeroCanvas.js
@@ -2,6 +2,7 @@ import useWindowScrollPosition from "@rehooks/window-scroll-position"
 import React, { useEffect, useRef, useState } from "react"
 import styled from "styled-components"
 import { Particle } from "./Particle"
+import useMousePosition from "../../../utils/useMousePosition"
 
 const StyledCanvas = styled.canvas`
   background-color: var(--black);
@@ -33,21 +34,44 @@ const HeroCanvas = () => {
   }
   const handleResize = e => {
     if (!canvasRef.current) return
-    console.log("CANVAS CURRENT: ", canvasRef.current)
     let ratio = getPixelRatio(canvasRef.current.getContext("2d"))
     canvasRef.current.width = e.target.innerWidth * ratio
     canvasRef.current.height = e.target.innerHeight * ratio
     init()
   }
-  const handleMouseMove = e => {
-    setMouse(prevState => ({ ...prevState, x: e.x, y: e.y }))
+
+  const connectParticles = () => {
+    for (let a = 0; a < particles.length; a++) {
+      for (let b = 0; b < particles.length; b++) {
+        let distanceX = particles[a].x - particles[b].x
+        let distanceY = particles[a].y - particles[b].y
+        let distance = distanceX ** 2 + distanceY ** 2
+        if (
+          distance <
+          ((canvasRef.current.width / 7) * canvasRef.current.height) / 7
+        ) {
+          canvasRef.current.getContext("2d").strokeStyle = "#2fbc7911"
+          canvasRef.current.getContext("2d").save()
+          canvasRef.current.getContext("2d").lineWidth = distance / 20000
+          canvasRef.current.getContext("2d").beginPath()
+          canvasRef.current
+            .getContext("2d")
+            .moveTo(particles[a].x, particles[a].y)
+          canvasRef.current
+            .getContext("2d")
+            .lineTo(particles[b].x, particles[b].y)
+          canvasRef.current.getContext("2d").stroke()
+          canvasRef.current.getContext("2d").restore()
+        }
+      }
+    }
   }
 
   const init = () => {
     if (!canvasRef.current) return
     setParticles([])
     let numberOfParticles = Math.floor(
-      (window.innerHeight * window.innerWidth) / 5000
+      (window.innerHeight * window.innerWidth) / 9000
     )
     const ctx = canvasRef.current.getContext("2d")
     let ratio = getPixelRatio(ctx)
@@ -58,17 +82,18 @@ const HeroCanvas = () => {
     let radius = (window.innerHeight / 80) * (window.innerWidth / 80)
     setMouse(prevState => ({ ...prevState, radius }))
     for (let i = 0; i < numberOfParticles; i++) {
-      let size = Math.random() * 5 + 1
+      let size = Math.random() * 30 + 1
       let x =
         Math.random() * (window.innerWidth - size * 2 - size * 2) + size * 2
       let y =
         Math.random() * (window.innerHeight - size * 2 - size * 2) + size * 2
-      let xVelocity = Math.random() * 5 - 2.5
-      let yVelocity = Math.random() * 5 - 2.5
-      let color = "rgba(255, 255, 255, 0.1)"
+      let xVelocity = Math.random() - 0.5
+      let yVelocity = Math.random() - 0.5
+      let color = "#2fbc7922"
       setParticles(particles => [
         ...particles,
         new Particle(
+          particles,
           canvasRef.current,
           canvasRef.current.getContext("2d"),
           x,
@@ -89,6 +114,7 @@ const HeroCanvas = () => {
     for (let i = 0; i < particles.length; i++) {
       particles[i].update()
     }
+    connectParticles()
   }
 
   const tick = () => {
@@ -97,25 +123,32 @@ const HeroCanvas = () => {
     requestAnimationFrame(tick)
   }
 
+  const handleMouseOut = () => {
+    setMouse(prevState => ({ ...prevState, x: undefined, y: undefined }))
+  }
+
   useEffect(() => {
-    init()
-    requestIdRef.current = requestAnimationFrame(tick)
-    window.addEventListener("resize", e => handleResize(e))
-    // window.addEventListener("mousemove", e => handleMouseMove(e))
+    if (canvasRef.current) {
+      init()
+      requestIdRef.current = requestAnimationFrame(tick)
+      window.addEventListener("resize", e => handleResize(e))
+      window.addEventListener("mousemove", e => {
+        console.log({ ...mouse, x: e.x, y: e.y })
+        setMouse(prevState => ({ ...prevState, x: e.x, y: e.y }))
+      })
+      window.addEventListener("mouseout", handleMouseOut)
+    }
 
     return () => {
       window.removeEventListener("resize", e => handleResize(e))
+      window.removeEventListener("mousemove", e =>
+        setMouse(prevState => ({ ...prevState, x: e.x, y: e.y }))
+      )
+      window.removeEventListener("mouseout", handleMouseOut)
       cancelAnimationFrame(requestIdRef.current)
       // window.removeEventListener("mousemove", e => handleMouseMove(e))
     }
-
-    // console.log("USEEFFECT")
-    // if (typeof window === "undefined" || !window.document) {
-    //   return
-    // }
-    // init()
-    // requestAnimationFrame(tick)
-  }, [])
+  }, [canvasRef.current])
 
   return <StyledCanvas ref={canvasRef} id="heroCanvas" />
 }
--- src/components/organisms/HomeHero/Particle.js
+++ src/components/organisms/HomeHero/Particle.js
@@ -8,6 +8,7 @@ import {
 
 export class Particle {
   constructor(
+    particles,
     canvas,
     canvasCtx,
     x,
@@ -18,6 +19,7 @@ export class Particle {
     color,
     mouse
   ) {
+    this.particles = particles
     this.canvas = canvas
     this.c = canvasCtx
     this.x = x
@@ -32,47 +34,48 @@ export class Particle {
   draw() {
     this.c.beginPath()
     this.c.arc(this.x, this.y, this.size, 0, Math.PI * 2, false)
-    this.c.fillStyle = this.color
-    this.c.fill()
+    this.c.strokeStyle = this.color
+    this.c.lineWidth = 2
+    this.c.stroke()
   }
 
   update() {
     this.draw()
 
-    if (this.x > this.canvas.width || this.x < 0) {
+    if (this.x > this.canvas.width - this.size || this.x < this.size) {
       this.xVelocity = -this.xVelocity
     }
 
-    if (this.y > this.canvas.height || this.y < 0) {
+    if (this.y > this.canvas.height - this.size || this.y < this.size) {
       this.yVelocity = -this.yVelocity
     }
 
-    // checking collision between two particles
-    let dx = this.mouse.x - this.x
-    let dy = this.mouse.y - this.y
+    // checking collision between mouse and particle
+    // let dx = this.mouse.x - this.x
+    // let dy = this.mouse.y - this.y
 
-    let distance = Math.sqrt(dx ** 2 + dy ** 2)
+    // let distance = Math.sqrt(dx ** 2 + dy ** 2)
 
-    if (distance < this.mouse.radius + this.size) {
-      if (
-        this.mouse.x < this.x &&
-        this.x < this.canvas.width - this.size * 10
-      ) {
-        this.x += 10
-      }
-      if (this.mouse.x > this.x && this.x > this.size * 10) {
-        this.x -= 10
-      }
-      if (
-        this.mouse.y < this.y &&
-        this.y < this.canvas.height - this.size * 10
-      ) {
-        this.y += 10
-      }
-      if (this.mouse.y > this.y && this.y > this.size * 10) {
-        this.y -= 10
-      }
-    }
+    // if (distance < this.mouse.radius + this.size) {
+    //   if (
+    //     this.mouse.x < this.x &&
+    //     this.x < this.canvas.width - this.size * 10
+    //   ) {
+    //     this.x += 10
+    //   }
+    //   if (this.mouse.x > this.x && this.x > this.size * 10) {
+    //     this.x -= 10
+    //   }
+    //   if (
+    //     this.mouse.y < this.y &&
+    //     this.y < this.canvas.height - this.size * 10
+    //   ) {
+    //     this.y += 10
+    //   }
+    //   if (this.mouse.y > this.y && this.y > this.size * 10) {
+    //     this.y -= 10
+    //   }
+    // }
     this.x += this.xVelocity
     this.y += this.yVelocity
     this.draw()
