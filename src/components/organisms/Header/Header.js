0f74952c40b5ef3617bb49f466c39261ee6836ee
jakubchrapek <chrapekuba@gmail.com>
2021-08-04 18:02:33 +0200

    Missing import

--- src/components/organisms/Header/Header.js
+++ src/components/organisms/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import PropTypes from "prop-types"
 import { useLocation } from "@reach/router"
 import { AnimatePresence } from "framer-motion"
