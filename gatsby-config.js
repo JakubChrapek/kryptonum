a484fba7d58c84a74332b2fcba0ecda521f73826
jakubchrapek <chrapekuba@gmail.com>
2021-08-19 07:06:21 +0200

    FB pixel integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,12 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-facebook-pixel`,
+      options: {
+        pixelId: "359506342387829",
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
