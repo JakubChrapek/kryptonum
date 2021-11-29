ee9f349b0b6c3f90ab074e41376130e101dc644e
jakubchrapek <chrapekuba@gmail.com>
2021-11-29 15:11:04 +0100

    New gtag id

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-0K8MM28F1N", // Google Analytics / GA
+          "UA-204248023-1", // Google Analytics / GA
         ],
         // This object is used for configuration specific to this plugin
         pluginConfig: {
