2afdb32a5aada8ba41e4bab895898c7679d988ee
kryptonumstudio <kryptonumstudio@gmail.com>
2023-05-13 12:08:09 +0200

    add coma

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-plugin-force-trailing-slashes`
+    `gatsby-plugin-force-trailing-slashes`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
